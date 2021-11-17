28609b5ab2505afb7221be24909b92ee9f943485
Arianna Johnson <ariannajohnson588@gmail.com>
2021-11-17 17:30:05 -0500

    iphone css

---
+++ frontend/src/components/Iphone/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+const Iphone = () => {
+    return (
+        <div className='placement'>
+            <div className='iphone'>
+                <div className='screen'></div>
+                <div className='home'></div>
+                </div>
+            </div>
+    )
+};
+
+export default Iphone;
