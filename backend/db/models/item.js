84278aed7dfcb83cc0eae55e07ea381e62004841
JM <j.maxwell.alan@gmail.com>
2021-11-17 14:39:33 -0500

    formattting

--- backend/db/models/item.js
+++ backend/db/models/item.js
@@ -1,28 +1,35 @@
 'use strict';
 
-const { Model, DATE } = require('sequelize');
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, { DataTypes, fn }) => {
-    class Item extends Model {
-        static associate({ User, Account }) {
-            Item.belongsTo(User, { foreignKey: 'user_id' })
-        }
+  class Item extends Model {
+    static associate ({ User, Account }) {
+      Item.belongsTo(User, { foreignKey: 'user_id' });
     }
+  }
 
-    Item.init({
-        name: { type: DataTypes.STRING(50), allowNull: false },
-        is_expense: DataTypes.BOOLEAN,
-        description: { type: DataTypes.TEXT, allowNull: false },
-        date_expected: { type: DataTypes.DATE, allowNull: false },
-        is_recurring: DataTypes.BOOLEAN,
-        amount: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
-        category: { type: DataTypes.STRING(50), allowNull: false },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: { model: 'Users' }
-        }
-
-    }, { sequelize, modelName: 'Items' });
-    return Item;
-}
+  Item.init({
+    name: { type: DataTypes.STRING(50), allowNull: false },
+    is_expense: DataTypes.BOOLEAN,
+    description: { type: DataTypes.TEXT, allowNull: false },
+    date_expected: { type: DataTypes.DATE, allowNull: false },
+    is_recurring: DataTypes.BOOLEAN,
+    amount: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+    category: { type: DataTypes.STRING(50), allowNull: false },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'Users' }
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('NOW')
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: fn('NOW')
+    }
+  }, { sequelize, modelName: 'Items' });
+  return Item;
+};
