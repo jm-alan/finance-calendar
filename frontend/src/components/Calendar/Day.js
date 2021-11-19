6de1cb191ec584c4688106a7296fd9e683ea3273
JM <j.maxwell.alan@gmail.com>
2021-11-19 16:18:08 -0500

    days self-align to grid

--- frontend/src/components/Calendar/Day.js
+++ frontend/src/components/Calendar/Day.js
@@ -1,7 +1,12 @@
-export default function Day ({ date }) {
+export default function Day ({ date, gridColumnStart }) {
   return (
-    <div className='calendar_day'>
-      {date}
+    <div
+      className='calendar_day'
+      style={{
+        gridColumnStart
+      }}
+    >
+      {date.split('/')[1]}
     </div>
   );
 }
