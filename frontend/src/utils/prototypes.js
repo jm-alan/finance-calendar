bedc33b70a31a128a1c9daa29260c73a02c21b01
JM <j.maxwell.alan@gmail.com>
2021-11-19 15:46:26 -0500

    create Date#toEnumeratedMonthObject

--- frontend/src/utils/prototypes.js
+++ frontend/src/utils/prototypes.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-extend-native */
+
+Date.prototype.toEnumeratedMonthObject = function () {
+  const origDate = this.getDate();
+  const monthObj = {};
+
+  this.setDate(1);
+  monthObj[this.toLocaleDateString({}, { dateStyle: 'short' })] = this.getDay();
+  this.setDate(2);
+  while (this.getDate() !== 1) {
+    monthObj[this.toLocaleDateString({}, { dateStyle: 'short' })] = this.getDay();
+    this.setDate(this.getDate() + 1);
+  }
+
+  this.setDate(origDate);
+  return monthObj;
+};
