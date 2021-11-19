943f2e99581f27fbd8b2fc0f1ef159c209bf707a
JM <j.maxwell.alan@gmail.com>
2021-11-19 16:18:54 -0500

    day -> weekday, mount new day component

--- frontend/src/components/Calendar/index.js
+++ frontend/src/components/Calendar/index.js
@@ -1,16 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import Day from './Day';
+
 import './index.css';
 
 export default function Calendar () {
+  const dispatch = useDispatch();
+
+  const year = useSelector(state => state.calendar.year);
+  const month = useSelector(state => state.calendar.month);
+
   return (
     <div className='calendar_container'>
       <div className='weekday_container'>
-        <div className='day'>Sun</div>
-        <div className='day'>Mon</div>
-        <div className='day'>Tue</div>
-        <div className='day'>Wed</div>
-        <div className='day'>Thu</div>
-        <div className='day'>Fri</div>
-        <div className='day'>Sat</div>
+        <div className='weekday'>Sun</div>
+        <div className='weekday'>Mon</div>
+        <div className='weekday'>Tue</div>
+        <div className='weekday'>Wed</div>
+        <div className='weekday'>Thu</div>
+        <div className='weekday'>Fri</div>
+        <div className='weekday'>Sat</div>
+      </div>
+      <div className='date_container'>
+        {Object.entries((new Date(year, month)).toEnumeratedMonthObject()).map(([date, weekday], idx) => (
+          <Day key={idx} date={date} gridColumnStart={weekday + 1} />
+        ))}
       </div>
     </div>
   );
