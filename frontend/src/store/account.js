51d81668a73d39fb182061014ac0c2a4237ea256
Arianna Johnson <ariannajohnson588@gmail.com>
2021-11-19 16:13:15 -0500

    thunks for accounts

--- frontend/src/store/account.js
+++ frontend/src/store/account.js
@@ -1,16 +1,17 @@
 // constants
 const GET_ACCOUNTS = 'account/GET_ACCOUNTS';
 const CREATE_ACCOUNTS = 'accounts/CREATE_ACCOUNTS';
+const DELETE_REVIEWS = 'accounts/DELETE_ACCOUNTS';
 
 // actions
 const getAccounts = (accounts) => ({ type: GET_ACCOUNTS, payload: accounts });
 const newAccount = (account) => ({ type: CREATE_ACCOUNTS, payload: account });
-
+const deleteAccount = (account) => ({ type: CREATE_ACCOUNTS, payload: account });
 
 
 // thunk actions
-export const getAllAccounts = (accountId) => async (dispatch) => {
-    const res = await fetch(`/api/accounts/`)
+export const getAccountsByUser = (userId) => async (dispatch) => {
+    const res = await fetch(`/api/accounts/user/${userId}`)
     const data = await res.json();
     dispatch(getAccounts(data));
 }
@@ -28,6 +29,18 @@ export const createAccount = (accountObj) => async (dispatch) => {
     dispatch(newAccount(data))
 }
 
+export const deleteAccountById = (id) => async (dispatch) => {
+    const res = await fetch(`/api/accounts/${id}/`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    const data = await res.json();
+    console.log("deleted", data);
+    dispatch(deleteAccount(data))
+}
+
 // reducer
 export default function reducer(state = {}, action) {
     let newState = {};
@@ -41,6 +54,10 @@ export default function reducer(state = {}, action) {
         case CREATE_ACCOUNTS:
             newState = { ...state };
             newState[action.payload.account.id] = action.payload.account;
+            return newState;
+        case DELETE_REVIEWS:
+            newState = { ...state };
+            delete newState[action.payload.id];
             return newState;
         default:
             return state;
