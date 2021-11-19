53e2f626ada6644c0197b62d1e2c73f874b79ae2
Arianna Johnson <ariannajohnson588@gmail.com>
2021-11-19 15:52:25 -0500

    create accounts thunk

--- frontend/src/store/account.js
+++ frontend/src/store/account.js
@@ -1,14 +1,31 @@
 // constants
 const GET_ACCOUNTS = 'account/GET_ACCOUNTS';
+const CREATE_ACCOUNTS = 'accounts/CREATE_ACCOUNTS';
 
 // actions
 const getAccounts = (accounts) => ({ type: GET_ACCOUNTS, payload: accounts });
+const newAccount = (account) => ({ type: CREATE_ACCOUNTS, payload: account });
+
+
 
 // thunk actions
 export const getAllAccounts = (accountId) => async (dispatch) => {
     const res = await fetch(`/api/accounts/`)
     const data = await res.json();
     dispatch(getReviews(data));
+}
+
+export const createAccount = (accountObj) => async (dispatch) => {
+    const { name, balance, historicBalance } = accountObj;
+    const res = await fetch(`/api/reviews/accounts/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, balance, historicBalance })
+    })
+    const data = await res.json();
+    dispatch(newAccount(data))
 }
 
 // reducer
@@ -21,7 +38,10 @@ export default function reducer(state = {}, action) {
                 newState[account.id] = account;
             })
             return newState;
-
+        case CREATE_ACCOUNTS:
+            newState = { ...state };
+            newState[action.payload.account.id] = action.payload.account;
+            return newState;
         default:
             return state;
     }
