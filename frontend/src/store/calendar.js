6a0d192ed5c6faacbc9f51722b80ccb14761c719
JM <j.maxwell.alan@gmail.com>
2021-11-19 15:30:22 -0500

    create previous and next month actions

--- frontend/src/store/calendar.js
+++ frontend/src/store/calendar.js
@@ -1,7 +1,27 @@
+const NEXT_MONTH = 'calendar/MONTH/NEXT';
+const PREV_MONTH = 'calendar/MONTH/PREV';
+
+export const NextMonth = () => ({
+  type: NEXT_MONTH
+});
+export const PrevMonth = () => ({
+  type: PREV_MONTH
+});
+
 export default function reducer (
-  state, { type }
+  state = { month: (new Date()).getMonth() }, { type }
 ) {
   switch (type) {
+    case NEXT_MONTH:
+      return {
+        ...state,
+        month: state.month === 11 ? 0 : state.month + 1
+      };
+    case PREV_MONTH:
+      return {
+        ...state,
+        month: state.month === 0 ? 11 : state.month - 1
+      };
     default:
       return state;
   }
