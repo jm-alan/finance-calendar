fd90470c64b945d2f4ed3758b4cfab48ae66c074
JM <j.maxwell.alan@gmail.com>
2021-11-19 20:04:55 -0500

    create route for items by id

--- backend/router/api/accounts.js
+++ backend/router/api/accounts.js
@@ -5,10 +5,24 @@ import restoreOrReject from '../../utils/restoreOrReject';
 const router = Router();
 
 router.get('/', restoreOrReject, asyncHandler(async (req, res) => {
-    const { user } = req;
-    const accounts = await user.getAccounts();
-    res.json({ accounts })
+  const { user } = req;
+  const accounts = await user.getAccounts();
+  res.json({ accounts });
 }));
 
+router.get('/:id(\\d+)/items', restoreOrReject, asyncHandler(async (req, res) => {
+  const { user, params: { id } } = req;
+  const account = await user.findAccountByPk(id);
+  if (!account) {
+    res.status(404).json({
+      errors: [
+        'An account with that ID belonging to this user was not found in the database.'
+      ]
+    });
+  }
+
+  const items = (await account.getItems()).toKeyedObject('id');
+  res.json({ items });
+}));
 
 export default router;
