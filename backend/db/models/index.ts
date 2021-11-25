f950fa89e0e3a804654d57e38c0d97012440960c
josh <j.maxwell.alan@gmail.com>
2021-11-25 14:08:27 -0500

    correct model index with intentional circular import

--- backend/db/models/index.ts
+++ backend/db/models/index.ts
@@ -1,15 +1,13 @@
 'use strict';
 
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize } from 'sequelize';
 import config from '../../config/database';
-import createUserClass from './user';
-import createAccountClass from './account';
-import createItemClass from './item';
+import User from './user';
+import Account from './account';
+import Item from './item';
 
 export const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, config)
   : new Sequelize(config.database, config.username, config.password, config);
 
-export const User = createUserClass(sequelize, DataTypes);
-export const Account = createAccountClass(sequelize, DataTypes);
-export const Item = createItemClass(sequelize, DataTypes);
+[User, Account, Item].forEach(model => model.associate({ User, Account, Item }))
