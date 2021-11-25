113b42d69606a3a40d2f2a4970f026a81f4aca9a
josh <j.maxwell.alan@gmail.com>
2021-11-25 15:54:33 -0500

    its...ALIVE!

--- backend/db/models/after.ts
+++ backend/db/models/after.ts
@@ -2,9 +2,11 @@ import User from './user';
 import Account from './account';
 import Item from './item';
 
-User.hasMany(Account, { foreignKey: 'user_id' });
-User.hasMany(Item, { foreignKey: 'user_id' });
-Account.belongsTo(User, { foreignKey: 'user_id' });
-Account.hasMany(Item, { foreignKey: 'account_id' });
-Item.belongsTo(User, { foreignKey: 'user_id' });
-Item.belongsTo(Account, { foreignKey: 'account_id' });
+export default () => {
+  User.hasMany(Account, { foreignKey: 'user_id' });
+  User.hasMany(Item, { foreignKey: 'user_id' });
+  Account.belongsTo(User, { foreignKey: 'user_id' });
+  Account.hasMany(Item, { foreignKey: 'account_id' });
+  Item.belongsTo(User, { foreignKey: 'user_id' });
+  Item.belongsTo(Account, { foreignKey: 'account_id' });
+}
