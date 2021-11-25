9ab1d29cfef7de3aa95aa7d186198f22a8ec5780
josh <j.maxwell.alan@gmail.com>
2021-11-25 14:44:10 -0500

    js -> ts

--- backend/db/seeders/20210429142508-default-users.js
+++ backend/db/seeders/20210429142508-default-users.js
@@ -2,13 +2,13 @@
 const { hashSync } = require('bcryptjs');
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface) => {
     await queryInterface.bulkInsert('Users', [
       { firstName: 'demo', email: 'demo@website.io', password: hashSync('password') }
     ]);
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     await queryInterface.bulkDelete('Users', null, {});
   }
 };
