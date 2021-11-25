dcbe1832b540487bfab69448dbff73e9dfb33601
josh <j.maxwell.alan@gmail.com>
2021-11-25 01:12:36 -0500

    correct model index exports for typing

--- backend/db/models/index.ts
+++ backend/db/models/index.ts
@@ -1,21 +1,15 @@
 'use strict';
 
-import { Sequelize } from 'sequelize';
+import { Sequelize, DataTypes } from 'sequelize';
 import config from '../../config/database';
 import createUserClass from './user';
 import createAccountClass from './account';
 import createItemClass from './item';
 
-const sequelize = process.env.DATABASE_URL
+export const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, config)
   : new Sequelize(config.database, config.username, config.password, config);
 
-const db = {
-  User: require('./user')(sequelize, Sequelize),
-  Account: require('./account')(sequelize, Sequelize),
-  Item: require('./item')(sequelize, Sequelize),
-  sequelize,
-  Sequelize
-};
-
-module.exports = db;
+export const User = createUserClass(sequelize, DataTypes);
+export const Account = createAccountClass(sequelize, DataTypes);
+export const Item = createItemClass(sequelize, DataTypes);
