48a93bab07c30e7ab5cf4286e93df1f5f64235cb
josh <j.maxwell.alan@gmail.com>
2021-11-25 14:21:22 -0500

    correctly reimplement account per sequelize docs

--- backend/db/models/account.ts
+++ backend/db/models/account.ts
@@ -1,37 +1,63 @@
 'use strict';
 
-import type { DataTypes, Sequelize } from 'sequelize/types'
+import type { Optional } from "sequelize/types";
 import type { AggregateModels } from './utilTypes';
-import { Model } from 'sequelize';
 
-export default (sequelize: Sequelize, { INTEGER, DECIMAL, STRING, TEXT }: typeof DataTypes) => {
-  class Account extends Model {
-    static associate({ User, Item }: AggregateModels) {
-      Account.belongsTo(User, { foreignKey: 'user_id' });
-      Account.hasMany(Item, { foreignKey: 'account_id' });
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '.';
+
+interface AccountAttributes {
+  id: number;
+  name: string;
+  balance: number;
+  historic_balance: string;
+  user_id: number;
+}
+
+interface AccountCreationAttributes extends Optional<AccountAttributes, 'id'> { };
+
+const { INTEGER, DECIMAL, STRING, TEXT } = DataTypes;
+
+export default class Account
+  extends Model<AccountAttributes, AccountCreationAttributes>
+  implements AccountAttributes {
+  public id: number;
+  public name: string;
+  public balance: number;
+  public historic_balance: string;
+  public user_id: number;
+
+  public readonly createdAt: Date;
+  public readonly updatedAt: Date;
+
+  static associate({ User, Item }: AggregateModels) {
+    Account.belongsTo(User, { foreignKey: 'user_id' });
+    Account.hasMany(Item, { foreignKey: 'account_id' });
+  }
+}
+
+Account.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: STRING(50),
+    allowNull: false
+  },
+  balance: {
+    type: DECIMAL(10, 2),
+    allowNull: false
+  },
+  historic_balance: TEXT,
+  user_id: {
+    type: INTEGER,
+    references: {
+      model: 'Users'
     }
   }
-
-  Account.init({
-    name: {
-      type: STRING(50),
-      allowNull: false
-    },
-    balance: {
-      type: DECIMAL(10, 2),
-      allowNull: false
-    },
-    historic_balance: TEXT,
-    user_id: {
-      type: INTEGER,
-      references: {
-        model: 'Users'
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'Accounts'
-  });
-
-  return Account;
-};
+}, {
+  sequelize,
+  modelName: 'Accounts'
+});
