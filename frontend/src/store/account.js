d2de1bda3d7ff9cbfa374d1ce273308e04d194cd
josh <j.maxwell.alan@gmail.com>
2021-11-25 21:36:59 -0500

    preferential formatting

--- frontend/src/store/account.js
+++ frontend/src/store/account.js
@@ -8,58 +8,57 @@ const getAccounts = (accounts) => ({ type: GET_ACCOUNTS, payload: accounts });
 const newAccount = (account) => ({ type: CREATE_ACCOUNTS, payload: account });
 const deleteAccount = (account) => ({ type: CREATE_ACCOUNTS, payload: account });
 
-
 // thunk actions
 export const getAllAccounts = () => async (dispatch) => {
-    const res = await fetch(`/api/accounts/`)
-    const data = await res.json();
-    dispatch(getAccounts(data));
-}
+  const res = await fetch('/api/accounts/');
+  const data = await res.json();
+  dispatch(getAccounts(data));
+};
 
 export const createAccount = (accountObj) => async (dispatch) => {
-    const { name, balance, historicBalance } = accountObj;
-    const res = await fetch(`/api/reviews/accounts/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name, balance, historicBalance })
-    })
-    const data = await res.json();
-    dispatch(newAccount(data))
-}
+  const { name, balance, historicBalance } = accountObj;
+  const res = await fetch('/api/reviews/accounts/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, balance, historicBalance })
+  });
+  const data = await res.json();
+  dispatch(newAccount(data));
+};
 
 export const deleteAccountById = (id) => async (dispatch) => {
-    const res = await fetch(`/api/accounts/${id}/`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-    const data = await res.json();
-    console.log('deleted', data);
-    dispatch(deleteAccount(data))
-}
+  const res = await fetch(`/api/accounts/${id}/`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const data = await res.json();
+  console.log('deleted', data);
+  dispatch(deleteAccount(data));
+};
 
 // reducer
-export default function reducer(state = {}, action) {
-    let newState = {};
-    switch (action.type) {
-        case GET_ACCOUNTS:
-            newState = { ...state };
-            action.payload.accounts.forEach(account => {
-                newState[account.id] = account;
-            })
-            return newState;
-        case CREATE_ACCOUNTS:
-            newState = { ...state };
-            newState[action.payload.account.id] = action.payload.account;
-            return newState;
-        case DELETE_REVIEWS:
-            newState = { ...state };
-            delete newState[action.payload.id];
-            return newState;
-        default:
-            return state;
-    }
+export default function reducer (state = {}, action) {
+  let newState = {};
+  switch (action.type) {
+    case GET_ACCOUNTS:
+      newState = { ...state };
+      action.payload.accounts.forEach(account => {
+        newState[account.id] = account;
+      });
+      return newState;
+    case CREATE_ACCOUNTS:
+      newState = { ...state };
+      newState[action.payload.account.id] = action.payload.account;
+      return newState;
+    case DELETE_REVIEWS:
+      newState = { ...state };
+      delete newState[action.payload.id];
+      return newState;
+    default:
+      return state;
+  }
 }
