43ec685021334af98aacf6ae87ad61167a8b7369
JM <j.maxwell.alan@gmail.com>
2021-11-26 18:10:22 -0500

    eliminate unnecessary state type import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
-import type { State } from './utils/types';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
 import Profile from './components/Profile';
