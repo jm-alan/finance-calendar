c382b0ce34f8ce003a0c3024f29b3f3d144ed648
JM <j.maxwell.alan@gmail.com>
2021-11-26 14:40:03 -0500

    migrate accounts array -> accounts object transformation to backend

--- backend/router/api/accounts.ts
+++ backend/router/api/accounts.ts
@@ -5,25 +5,33 @@ import restoreOrReject from '../../utils/restoreOrReject';
 
 const router = Router();
 
-router.get('/', restoreOrReject, asyncHandler(async (req: AuthenticatedRequest, res) => {
-  const { user } = req;
-  const accounts = await user.getAccounts();
-  res.json({ accounts });
-}));
+router.get(
+  '/',
+  restoreOrReject,
+  asyncHandler(async (req: AuthenticatedRequest, res) => {
+    const { user } = req;
+    const accounts = (await user.getAccounts()).toKeyedObject('id');
+    res.json({ accounts });
+  })
+);
 
-router.get('/:id(\\d+)/items', restoreOrReject, asyncHandler(async (req: AuthenticatedRequest, res) => {
-  const { user, params: { id } } = req;
-  const account = await user.findAccountByPk(+id);
-  if (!account) {
-    res.status(404).json({
-      errors: [
-        'An account with that ID belonging to this user was not found in the database.'
-      ]
-    });
-  }
+router.get(
+  '/:id(\\d+)/items',
+  restoreOrReject,
+  asyncHandler(async (req: AuthenticatedRequest, res) => {
+    const { user, params: { id } } = req;
+    const account = await user.findAccountByPk(+id);
+    if (!account) {
+      res.status(404).json({
+        errors: [
+          'An account with that ID belonging to this user was not found in the database.'
+        ]
+      });
+    }
 
-  const items = (await account.getItems()).toKeyedObject('id');
-  res.json({ items });
-}));
+    const items = (await account.getItems()).toKeyedObject('id');
+    res.json({ items });
+  })
+);
 
 export default router;
