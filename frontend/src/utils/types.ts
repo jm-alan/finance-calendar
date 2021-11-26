b18448cbd77a3e4b4b31384029c318a39dc17592
josh <j.maxwell.alan@gmail.com>
2021-11-25 21:37:16 -0500

    create types file

---
+++ frontend/src/utils/types.ts
@@ -0,0 +1,24 @@
+export type State = {
+  session: {
+    user: any,
+    loaded: boolean
+  };
+  accounts: {
+    all: {
+      [key: number]: any
+    },
+    loaded: boolean
+  };
+  calendar: {
+    month: number,
+    year: number
+  };
+  errors: null | string[];
+  modal: {
+    Current: null | JSX.Element,
+    mooring: null | HTMLElement
+  },
+  UX: {
+    modal: boolean
+  }
+}
