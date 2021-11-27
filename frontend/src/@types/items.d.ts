653b8d0b05c488aa9cab044bbedf3e23a3e68106
JM <j.maxwell.alan@gmail.com>
2021-11-26 19:12:55 -0500

    create items reducer and corresponding type declarations

---
+++ frontend/src/@types/items.d.ts
@@ -0,0 +1,40 @@
+declare type ExtantItem = {
+  id: number;
+  name: string;
+  is_expense: boolean;
+  description: string;
+  date_expected: Date;
+  is_recurring: boolean;
+  amount: number;
+  category: string;
+  user_id: number;
+  account_id: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+declare type NewItem = {
+  name: string;
+  is_expense: boolean;
+  description: string;
+  date_expected: Date;
+  is_recurring: boolean;
+  amount: number;
+  category: string;
+};
+
+declare type ExtantItemCollection = {
+  [key: number]: ExtantItem | null;
+};
+
+declare type ItemState = {
+  all: ItemCollection;
+  loaded: boolean;
+};
+
+declare type ItemAction = {
+  type: 'items/ALL' | 'items/CREATE' | 'items/UPDATE' | 'items/DELETE';
+  item?: ExtantItem;
+  items?: ExtantItemCollection;
+  id?: number;
+};
