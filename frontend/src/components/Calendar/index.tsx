9c4b99fa9b4b862811a21ecabf9a63af764dfd79
JM <j.maxwell.alan@gmail.com>
2021-11-26 22:04:31 -0500

    move account controls to own component

--- frontend/src/components/Calendar/index.tsx
+++ frontend/src/components/Calendar/index.tsx
@@ -1,34 +1,20 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Day from './Day';
-import { NextMonth, PrevMonth } from '../../store/calendar';
+import Controls from './Controls';
 
 import './index.css';
 
 type DateTuple = [string, number];
 
 export default function Calendar () {
-  const dispatch = useDispatch();
 
   const year = useSelector((state: State) => state.calendar.year);
   const month = useSelector((state: State) => state.calendar.month);
 
   return (
     <div className='calendar_container'>
-      <div className='calendar_controls'>
-        <button
-          className='pop-button background-blue'
-          onClick={() => dispatch(PrevMonth())}
-        >
-          Prev
-        </button>
-        <button
-          className='pop-button background-blue'
-          onClick={() => dispatch(NextMonth())}
-        >
-          Next
-        </button>
-      </div>
+      <Controls />
       <div className='weekday_container'>
         <div className='weekday'>Sun</div>
         <div className='weekday'>Mon</div>
