d0386902862f61082542a90c05ffde56d65b7c9b
JM <j.maxwell.alan@gmail.com>
2021-11-26 19:10:54 -0500

    translate csrfetch back to class for privacy implementation

--- frontend/src/@types/csrfetch.d.ts
+++ frontend/src/@types/csrfetch.d.ts
@@ -16,23 +16,7 @@ declare type reqOpts = {
 declare type paramsObj = {
   [key: string]: any;
 };
+
 declare type bodyObj = {
   [key: string]: any;
-};
-
-declare type csrfetch = {
-  options: [
-    string,
-    reqOpts
-  ];
-  genericErrors: string[];
-  dispatch: Dispatch<ErrorAction> | (() => void);
-  captureDispatch: (dispatch: Dispatch<ErrorAction>) => void;
-  __preFlight: (opts: preflightOpts, method: string) => void;
-  __forwardFetch: (opts: preflightOpts, method?: string) => Promise<any>;
-  get: (url: string, params?: paramsObj) => Promise<any>;
-  post: (url: string, body: bodyObj) => Promise<any>;
-  patch: (url: string, body: bodyObj) => Promise<any>;
-  destroy: (url: string, body?: bodyObj) => Promise<any>;
-  restoreCSRF: () => Promise<void>;
 };
--- frontend/src/store/csrfetch.ts
+++ frontend/src/store/csrfetch.ts
@@ -4,27 +4,33 @@ import type { Dispatch } from 'redux';
 import findCookie from '../utils/findCookie';
 import { SetErrors } from './errors';
 
-const csrfetch: csrfetch = {
-  dispatch: () => {},
+class CsrfFetch {
+  private readonly options: [string, reqOpts];
+  private readonly genericErrors: string[];
+  private dispatch: Dispatch<ErrorAction> | (() => void) = () => {};
 
-  options: [
-    '',
-    {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: '',
-      method: ''
-    }
-  ],
+  constructor () {
+    this.options = [
+      '',
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: '',
+        method: ''
+      }
+    ];
+    this.genericErrors = [
+      'Sorry, something went wrong. Please refresh the page and try again.'
+    ];
+  }
 
-  genericErrors: ['Sorry, something went wrong. Please refresh the page and try again.'],
-
-  captureDispatch (dispatch: Dispatch<ErrorAction>) {
+  public captureDispatch (dispatch: Dispatch<ErrorAction>) {
     this.dispatch = dispatch;
-  },
+  }
 
-  __preFlight (opts = { url: '', params: null, body: null }, method) {
+
+  private __preFlight (opts: preflightOpts = { url: '', params: null, body: null }, method: string) {
     this.options[1].headers['XSRF-Token'] = findCookie('XSRF-TOKEN');
     if (method === 'GET') delete this.options[1].body;
     this.options[0] = opts.url;
@@ -35,9 +41,9 @@ const csrfetch: csrfetch = {
       for (const key in opts.params) opts.url += `&${key}=${opts.params[key]}`;
     }
     if (opts.body) this.options[1].body = JSON.stringify(opts.body);
-  },
+  }
 
-  async __forwardFetch (opts = { url: '', params: null, body: null }, method = 'GET') {
+  private async __forwardFetch (opts: preflightOpts = { url: '', params: null, body: null }, method = 'GET') {
     this.__preFlight(opts, method);
     const res = await window.fetch(...this.options);
     try {
@@ -47,27 +53,29 @@ const csrfetch: csrfetch = {
       this.dispatch(SetErrors(errors || this.genericErrors));
       return {};
     }
-  },
+  }
 
-  async get (url, params) {
+  async get (url: string, params?: paramsObj) {
     return await this.__forwardFetch({ url, params });
-  },
+  }
 
-  async post (url, body) {
+  async post (url: string, body: bodyObj) {
     return await this.__forwardFetch({ url, body }, 'POST');
-  },
+  }
 
-  async patch (url, body) {
+  async patch (url: string, body: bodyObj) {
     return await this.__forwardFetch({ url, body }, 'PATCH');
-  },
+  }
 
-  async destroy (url, body) {
+  async destroy (url: string, body?: bodyObj) {
     return await this.__forwardFetch({ url, body }, 'DELETE');
-  },
+  }
 
   async restoreCSRF () {
     await this.get('/api/csrf/restore/');
   }
-};
+}
+
+const csrfetch = new CsrfFetch();
 
 export default csrfetch;
