a14d01a139e535717e8da7bb5bc52fd8349b4f67
JM <j.maxwell.alan@gmail.com>
2021-11-27 01:35:33 -0500

    add literalMonth to calendar state

--- frontend/src/@types/calendar.d.ts
+++ frontend/src/@types/calendar.d.ts
@@ -1,6 +1,7 @@
 declare type CalendarState = {
   month: number;
   year: number;
+  literalMonth: 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December';
 };
 
 declare type CalendarAction = {
