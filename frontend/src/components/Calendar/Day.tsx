b1890e99b66e784ceea4a93d6d119d94c970d4c8
JM <j.maxwell.alan@gmail.com>
2021-11-27 17:03:20 -0500

    insert and rudimentarily style item amounts and positive/negative

--- frontend/src/components/Calendar/Day.tsx
+++ frontend/src/components/Calendar/Day.tsx
@@ -19,6 +19,9 @@ export default function Day ({ date, gridColumnStart }: DayProps) {
   const [height, setHeight] = useState<undefined | number>(undefined);
   const [width, setWidth] = useState<undefined | number>(undefined);
 
+  const net = items && Object.values(items).reduce((acc, next) => acc + (next.is_expense ? -next.amount : +next.amount), 0);
+  const neutralPositiveNegative = net === 0 ? '' : net > 0 ? ' positive' : ' negative';
+
   useEffect(() => {
     const boundingRect = dayRef.current && dayRef.current.getBoundingClientRect();
     if (boundingRect) {
@@ -48,14 +51,24 @@ export default function Day ({ date, gridColumnStart }: DayProps) {
         height
       }}
     >
-      <div className='calendar_date background-purple'>
-        {date.split('/')[1]}
+      <div className='calendar_day-header'>
+        <div className='calendar_date background-purple'>
+          {date.split('/')[1]}
+        </div>
+        <div className='calendar_day-net'>
+          Net: <span className={`daily-net${neutralPositiveNegative}`}>{net}</span>
+        </div>
       </div>
       {items && (
-        <div className='calendar_date-items-list'>
+        <div className='calendar_day-items-list'>
           {Object.values(items).map((item, idx) => (
-            <div className='calendar_date-item-entry' key={idx}>
-              {item.name}
+            <div className='calendar_day-item-entry' key={idx}>
+              <span className='item_entry_name'>
+                {item.name}
+              </span>
+              <span className={`item_entry_amount${(item.is_expense) ? ' negative' : ' positive'}`}>
+                {+item.amount}
+              </span>
             </div>
           ))}
         </div>
