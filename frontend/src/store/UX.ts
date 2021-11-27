e4c4d4d6acaf3aa1eeee8e5bb88113227fec40e0
JM <j.maxwell.alan@gmail.com>
2021-11-27 00:40:23 -0500

    create account dropdown expand/collapse actions

--- frontend/src/store/UX.ts
+++ frontend/src/store/UX.ts
@@ -1,31 +1,37 @@
 const SHOW_MODAL = 'UX/modal/SHOW';
 const HIDE_MODAL = 'UX/modal/HIDE';
+const EXPAND_ACCOUNTS = 'UX/accounts/EXPAND';
+const COLLAPSE_ACCOUNTS = 'UX/accounts/COLLAPSE';
 
-export type UXState = {
-  modal: boolean;
-};
-
-type UXAction = {
-  type: string;
-};
-
-export const ShowModal = () => ({
+export const ShowModal = (): UXAction => ({
   type: SHOW_MODAL
 });
 
-export const HideModal = () => ({
+export const HideModal = (): UXAction => ({
   type: HIDE_MODAL
 });
 
+export const ExpandAccounts = (): UXAction => ({
+  type: EXPAND_ACCOUNTS
+});
+
+export const CollapseAccounts = (): UXAction => ({
+  type: COLLAPSE_ACCOUNTS
+});
+
 export default function reducer (
-  state = { modal: false },
+  state: UXState = { modal: false, accountsDropdown: false },
   { type }: UXAction
-) {
+): UXState {
   switch (type) {
     case SHOW_MODAL:
       return { ...state, modal: true };
     case HIDE_MODAL:
       return { ...state, modal: false };
+    case EXPAND_ACCOUNTS:
+      return { ...state, accountsDropdown: true };
+    case COLLAPSE_ACCOUNTS:
+      return { ...state, accountsDropdown: false };
     default:
       return state;
   }
