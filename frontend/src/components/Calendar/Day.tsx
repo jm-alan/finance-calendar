ee1a8be1094a1513eb988d0fda769d6fb4e5517c
JM <j.maxwell.alan@gmail.com>
2021-11-27 02:34:58 -0500

    render unstyled items in calendar day

--- frontend/src/components/Calendar/Day.tsx
+++ frontend/src/components/Calendar/Day.tsx
@@ -11,6 +11,7 @@ export default function Day ({ date, gridColumnStart }: DayProps) {
   const dispatch = useDispatch();
 
   const selectedAccount = useSelector((state: State) => state.accounts.selected);
+  const items = useSelector((state: State) => state.items.byDate[date]) ?? null;
 
   useEffect(() => {
     const accountId = selectedAccount ? selectedAccount.id : 0;
@@ -30,6 +31,15 @@ export default function Day ({ date, gridColumnStart }: DayProps) {
       <div className='calendar_date background-purple'>
         {date.split('/')[1]}
       </div>
+      {items && (
+        <div className='calendar_date-items-list'>
+          {Object.values(items).map((item, idx) => (
+            <div className='calendar_date-item-entry' key={idx}>
+              {item.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
