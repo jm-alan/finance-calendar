ed1f276ac585dc1f8806ab62655ab48462b51086
JM <j.maxwell.alan@gmail.com>
2021-11-30 03:52:50 -0500

    open navbar on button click and close on offclick

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,14 +5,16 @@ import LoginForm from '../Auth/LoginForm';
 import SignupForm from '../Auth/SignupForm';
 import { SetModal } from '../../store/modal';
 import { LogOut } from '../../store/session';
-import { ShowModal } from '../../store/UX';
+import { Hidebar, ShowModal, Sidebar } from '../../store/UX';
 
 import './index.css';
+import { MouseEventHandler, useEffect } from 'react';
 
 export default function NavBar () {
   const dispatch = useDispatch();
 
   const user = useSelector((state: State) => state.session.user);
+  const sidebar = useSelector((state: State) => state.UX.navBar);
 
   const popLogin = () => {
     dispatch(SetModal(LoginForm));
@@ -26,32 +28,52 @@ export default function NavBar () {
 
   const logOut = () => dispatch(LogOut());
 
+  const resist: MouseEventHandler<HTMLElement> = e => e.stopPropagation();
+
+  useEffect(() => {
+    const hidebar = () => dispatch(Hidebar());
+    if (sidebar) document.addEventListener('click', hidebar);
+    return () => {
+      document.removeEventListener('click', hidebar);
+    };
+  }, [dispatch, sidebar]);
+
   return (
-    <nav>
-      {user
-        ? (
-          <>
-            <Link to='/'>
-              Home
-            </Link>
-            <Link to='/users/me/'>
-              My Profile
-            </Link>
-            <button onClick={logOut}>
-              Log Out
-            </button>
-          </>
-        )
-        : (
-          <div className='button_container'>
-            <button className='main_button' onClick={popLogin}>
-              Log In
-            </button>
-            <button className='main_button' onClick={popSignup}>
-              Sign Up
-            </button>
-          </div>
-        )}
-    </nav>
+    <>
+      <button
+        className='navbar_show_hide'
+        onClick={() => dispatch(!sidebar ? Sidebar() : Hidebar())}
+      >
+        <div className='navbar_hamburger' />
+        <div className='navbar_hamburger' />
+        <div className='navbar_hamburger' />
+      </button>
+      <nav onClick={resist} className={`${sidebar ? 'show' : ''}`}>
+        {user
+          ? (
+            <>
+              <Link to='/'>
+                Home
+              </Link>
+              <Link to='/users/me/'>
+                My Profile
+              </Link>
+              <button onClick={logOut}>
+                Log Out
+              </button>
+            </>
+          )
+          : (
+            <div className='button_container'>
+              <button className='main_button' onClick={popLogin}>
+                Log In
+              </button>
+              <button className='main_button' onClick={popSignup}>
+                Sign Up
+              </button>
+            </div>
+          )}
+      </nav>
+    </>
   );
 }
